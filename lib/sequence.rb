ec18e74a950828f655e23996dc0d48910afa8eae
Arthur Crymble <arthur.crymble@icloud.com>
2022-07-10 19:08:46 +0100

    Add PEGS hash to map letter representation to coloured icons

--- lib/sequence.rb
+++ lib/sequence.rb
@@ -1,6 +1,28 @@
 # frozen_string_literal: true
 
 # A sequence in the game Mastermind
-class sequence
+class Sequence
+  PEGS = {
+    'r' => "\e[38;5;9m\u2B24\e[0m",
+    'o' => "\e[38;5;215m\u2B24\e[0m",
+    'y' => "\e[38;5;228m\u2B24\e[0m",
+    'g' => "\e[38;5;10m\u2B24\e[0m",
+    'b' => "\e[38;5;12m\u2B24\e[0m",
+    'p' => "\e[38;5;13m\u2B24\e[0m"
+  }.freeze
 
+  def initialize(sequence)
+    @sequence = sequence
+  end
+
+  def to_s
+    string = ''
+    @sequence.split('').each do |peg|
+      string += PEGS[peg]
+    end
+    string
+  end
 end
+
+sq = Sequence.new('roygbp')
+puts sq
